1711c3586074999316efde5c3332526729dbe1fb
Tabah6 <74711146+Tabah6@users.noreply.github.com>
2021-04-05 17:01:02 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
-export KERNELNAME=Zaky_ma
+export KERNELNAME=Moonlight
 
-export LOCALVERSION=1
+export LOCALVERSION=Arashi-v2.3-TEST
 
-export KBUILD_BUILD_USER=Zaky_Ma
+export KBUILD_BUILD_USER=Tabah"HBT"
 
-export KBUILD_BUILD_HOST=.
+export KBUILD_BUILD_HOST=server
 
 export TOOLCHAIN=clang
 
-export DEVICES=whyred,tulip,wayne
+export DEVICES=whyred,tulip,lavender,a26x
+
+export CI_ID=-1001211283062
+
+export GROUP_ID=-1001420838318
 
 source helper
 
 gen_toolchain
 
-send_msg "Start building..."
+send_msg "⏳ Start building ${KERNELNAME} ${LOCALVERSION} | DEVICES: whyred - tulip - lavender - wayne - jasmine"
+
+send_pesan "⏳ Start building ${KERNELNAME} ${LOCALVERSION} | DEVICES: whyred - tulip - lavender - wayne - jasmine"
 
 START=$(date +"%s")
 
@@ -27,9 +33,13 @@ do
 	build ${i} -newcam
 done
 
-send_msg "Build OC..."
+send_msg "⏳ Start building Overclock version | DEVICES: whyred - tulip"
+
+send_pesan "⏳ Start building Overclock version | DEVICES: whyred - tulip"
 
 git apply oc.patch
+
+git apply em.patch
 
 for i in ${DEVICES//,/ }
 do
@@ -45,4 +55,6 @@ END=$(date +"%s")
 
 DIFF=$(( END - START ))
 
-send_msg "Build complete in $((DIFF / 60))m $((DIFF % 60))s"
+send_msg "✅ Build completed in $((DIFF / 60))m $((DIFF % 60))s,get TEST builds in @MoonlightCI | Linux version : $(make kernelversion) | Last commit: $(git log --pretty=format:'%s' -5)"
+
+send_pesan "✅ Build completed in $((DIFF / 60))m $((DIFF % 60))s | Linux version : $(make kernelversion) | Last commit: $(git log --pretty=format:'%s' -5)"
